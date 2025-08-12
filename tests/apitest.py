61b1387bb8f7d6cf865b27da8a009ae7ab96cdf4
WitherXee <witherxee@gmail.com>
2025-08-12 19:16:50 +0530

    Update apitest.py

--- tests/apitest.py
+++ tests/apitest.py
@@ -1,6 +1,6 @@
 import base64
 
-apikey = "9226b86f1aa00966b74d1fcde3263ddef0b27694"
+apikey = ""
 
 bytekey = apikey.encode()
 
@@ -8,3 +8,4 @@ api = open("Hunter_API.key", 'wb')
 api.write(base64.b64encode(bytekey))
 
 api.close()
+
